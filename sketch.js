c94a58e3b7e67e5fc103a72b6855b40218cb3281
harry <denton.harry@gmail.com>
2020-05-19 15:24:55 +0100

    laodDivs function creates fillerDivs inside container_Div but a further function to change fillerDivs doesn't work at the moment.

--- sketch.js
+++ sketch.js
@@ -1,26 +1,52 @@
-let container_Div = document.querySelector('#container');
+let container_Div = document.querySelector("#container");
+let fillerDivs = document.getElementById("fillerDivs");
 
-container_Div.style.border = "1px solid black";
+
 container_Div.style.width = "320px";
 container_Div.style.height = "320px";
-container_Div.style.display = "flex";
-container_Div.style.flexFlow = "row wrap";
 
-
-window.addEventListener("load", loadDivs);
 
     function loadDivs (){
 
         for (let i = 0; i < 256; i++){
 
-            const fillerDivs = document.createElement("div");
+            var fillerDivs = document.createElement("div");
+            fillerDivs.id = "fillerDivs";
 
             fillerDivs.classList.add("fillerDivs");
 
-            fillerDivs.style.background = "gray";
-            fillerDivs.style.height = "20px";
-            fillerDivs.style.width = "20px";
-
         container_Div.appendChild(fillerDivs);
+    }
+};
 
-    }};
+loadDivs();
+
+window.onload = function(){
+
+    fillerDivs.addEventListener("mouseover", function( event ) {
+
+        event.target.style.background = "orange";
+
+        setTimeout( function() {
+            event.target.style.background = "";
+        }, 2000);
+
+    }, false);
+
+}
+
+//just a test to make sure the above function works on static divs.
+
+let tester = document.getElementById("tester");
+
+  tester.addEventListener("mouseover", function( event ) {   
+
+    event.target.style.background = "orange";
+  
+
+    setTimeout(function() {
+      event.target.style.background = "";
+    }, 2000);
+  }, false);
+
+  
